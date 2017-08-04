e8b681b8d391842c9b4740f1b76e87890f465e5d
Juliano Padilha <sistemas.padilha@gmail.com>
2017-08-03 23:34:57 -0300

    Adiciona resoures do curso de React

---
+++ react/src/components/search_bar.js
@@ -0,0 +1,28 @@
+// Importa o React para ser usado na intepretação do
+// componente JSX (React transforma ele em React.createElement(...))
+import React from 'react';
+
+// Duas formas de criar um componente (function component e class component)
+// EXPLICAÇÃO:
+// Go with functional if your component doesn't do much more than take in some props and render.
+// You can think of these as pure functions because they will always render and behave the same,
+// given the same props. Also, they don't care about lifecycle methods or have their own internal state.
+// Because they're lightweight, writing these simple components as functional components is pretty standard.
+// If your components need more functionality, like keeping state, use classes instead.
+// https://facebook.github.io/react/docs/components-and-props.html
+
+// Exemplo de function component
+// const SearchBar = () => {
+// 	return <input />
+// }
+
+// Criamos uma class component e extendemos as funcionalidades do React para usar dentro da classe.
+// É obrigatório usar o render()
+class SearchBar extends React.Component {
+	render() {
+		return <input />;
+	}
+}
+
+// Exporta o componentes criado para ser usado em outras partes da aplicação
+export default SearchBar;
---
+++ react/src/index.js
@@ -0,0 +1,20 @@
+// Importa a biblioteca do React e ReactDOM (para renderizar os componentes no DOM).
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Importa o componente criado passando seu diretório de origem
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCQUQ8BS6Xvi0owKSLjZ8AoWEDe5o_WD_8';
+
+// Cria um novo componente utilizando JSX(<div></div>, neste caso) e ES6(arrow functions, neste caso)
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	)
+}
+
+// Renderiza o componente no DOM passando uma instância dele e onde queremos renderizá-lo
+ReactDOM.render(<App />, document.querySelector('.container'));
